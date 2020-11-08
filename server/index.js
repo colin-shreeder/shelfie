de61e7eeaf7cf3757498b9299953d1907a62f8eb
Colin Reeder <colin.c.reeder@gmail.com>
2020-11-07 17:38:47 -0700

    draft2

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require ('dotenv').config();
+const express = require("express");
+const massive = require('massive');
+const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }).then( db => {
+    app.set("db", db)
+    console.log("Well, that's like... your opinion... man")
+  })
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
+  });
